af691d4f770d78ddc1cb015c359288593e95324d
jstange20 <jstange20@gmail.com>
2016-04-29 13:56:01 -0700

    Added files via upload

--- Final-Deliverable/Game1.java
+++ Final-Deliverable/Game1.java
@@ -167,109 +167,109 @@ public class Game1
 			// 5 rolled dice were selected to be scored
 			// 6 rolled dice were selected to be scored
 		}
-	public void nextAction() 
-	{
-		String userInput = "";
 		
-		System.out.println("Press q: Quit and add your turn score to your game score");
-		System.out.println("Press r: Roll any dice that have not been used as point dice");
 		
-		userInput = userSelect.next();
-		
-		if (userInput.equals("q"))
-			// End the turn
-			;
-		else if (userInput.equals("r"))
-			// Roll again
-			;
-	}	
-	
-	public void quickSort(ArrayList<Die> aList)
-	{		
-		doQuickSort(aList);
-	}
-	public ArrayList<Die> doQuickSort(ArrayList<Die> array) 
-	{
-		// if array is of size 1, return it since no sorting is required
-		if(array.size() <= 1)
+		public void nextAction() 
 		{
+			String userInput = "";
+			
+			System.out.println("Press q: Quit and add your turn score to your game score");
+			System.out.println("Press r: Roll any dice that have not been used as point dice");
+			
+			userInput = userSelect.next();
+			
+			if (userInput.equals("q"))
+			// End the turn
+				;
+			else if (userInput.equals("r"))
+			// Roll again
+				;
+		}	
+	
+		public void quickSort(ArrayList<Die> aList)
+		{		
+			doQuickSort(aList);
+		}
+		public ArrayList<Die> doQuickSort(ArrayList<Die> array) 
+		{
+			// if array is of size 1, return it since no sorting is required
+			if(array.size() <= 1)
+			{
+				return array;
+			}
+			
+			// create lesser and greater arraylists to hold values less than & greater than pivot
+			ArrayList<Die> lesser = new ArrayList<Die>();
+			ArrayList<Die> greater = new ArrayList<Die>();
+			
+			int pivotIndex = array.size() - 1; // make the last element in the array the pivot point
+			int compareIndex = 0; // make the first element the compare point to start with
+			int storeIndex = 0; // make the first element the store point to start with
+			int lessIndex = 0; // initialize index for lesser (than pivot) array
+			int greatIndex = 0; // initialize index for greater (than pivot) array
+			
+			// Go through the whole array applying one round of Quicksort algorithm using pivot point
+			// 1) compare compare-point element with pivot-point element
+			// 2) if it is smaller or equal, add it to lesser arraylist, and then move compare-point to next element, and 
+			//    move store-point to next element
+			// 3) if it is greater, add element to greater arrraylist, and move compare-point to next element
+			// 4) repeat until end of array is reached
+			while (compareIndex < pivotIndex)
+			{
+				if((array.get(compareIndex)).getValue() <= (array.get(pivotIndex)).getValue())
+				{	// compare-point element is smaller than or equal to pivot-point element
+					// add the compare-point element to lesser arraylist
+					lesser.add(array.get(compareIndex));
+					lessIndex++;
+					compareIndex++;
+					storeIndex++;
+				}
+				else
+				{	// compare-point element is greater than pivot-point element
+					// add the compare-point element to greater arraylist
+					greater.add(array.get(compareIndex));
+					greatIndex++;
+					compareIndex++;
+				}
+			}
+			// Reached end of array where compare-point is at pivot-point. 		
+			// Start Recurring with lesser and greater arraylists
+			lesser = doQuickSort(lesser);
+			greater = doQuickSort(greater);
+		
+		
+			// At each round, put the partially sorted arraylist together until whole list is sorted using concatenate.
+			// the new concatenated is then rewritten to the array.
+			copy(concatenate(lesser, array.get(pivotIndex), greater), array);
 			return array;
 		}
-		
-		// create lesser and greater arraylists to hold values less than & greater than pivot
-		ArrayList<Die> lesser = new ArrayList<Die>();
-		ArrayList<Die> greater = new ArrayList<Die>();
-		
-		int pivotIndex = array.size() - 1; // make the last element in the array the pivot point
-		int compareIndex = 0; // make the first element the compare point to start with
-		int storeIndex = 0; // make the first element the store point to start with
-		int lessIndex = 0; // initialize index for lesser (than pivot) array
-		int greatIndex = 0; // initialize index for greater (than pivot) array
-		
-		// Go through the whole array applying one round of Quicksort algorithm using pivot point
-		// 1) compare compare-point element with pivot-point element
-		// 2) if it is smaller or equal, add it to lesser arraylist, and then move compare-point to next element, and 
-		//    move store-point to next element
-		// 3) if it is greater, add element to greater arrraylist, and move compare-point to next element
-		// 4) repeat until end of array is reached
-		while (compareIndex < pivotIndex)
-		{
-			if((array.get(compareIndex)).getValue() <= (array.get(pivotIndex)).getValue())
-			{// compare-point element is smaller than or equal to pivot-point element
-			 // add the compare-point element to lesser arraylist
-				lesser.add(array.get(compareIndex));
-				lessIndex++;
-				compareIndex++;
-				storeIndex++;
-			}
-			else
-			{// compare-point element is greater than pivot-point element
-			 // add the compare-point element to greater arraylist
-				greater.add(array.get(compareIndex));
-				greatIndex++;
-				compareIndex++;
-			}
-		}
-		// Reached end of array where compare-point is at pivot-point. 		
-		// Start Recurring with lesser and greater arraylists
-		lesser = doQuickSort(lesser);
-		greater = doQuickSort(greater);
-		
-		
-		// At each round, put the partially sorted arraylist together until whole list is sorted using concatenate.
-		// the new concatenated is then rewritten to the array.
-		copy(concatenate(lesser, array.get(pivotIndex), greater), array);
-		return array;
-	}
 	
-	// method used to copy the updated list to array.
-	public void copy( ArrayList<Die> one, ArrayList<Die> another)
-	{
-		for (int i =0; i< one.size(); i++)
+		// method used to copy the updated list to array.
+		public void copy( ArrayList<Die> one, ArrayList<Die> another)
 		{
-			another.set(i,(one.get(i)));
-		}
-		
-	}
-	
-	// Method to build concatenated list: lesser + pivot + greater
-	public  ArrayList<Die> concatenate(ArrayList<Die> lesser,Die pivot,  ArrayList<Die> greater)
-	{
-		// create a newlist to hold the concatenated arraylist
-		ArrayList<Die> newList = new ArrayList<Die>();
-		// add lesser portion to newlist		
-		for (int i = 0; i < lesser.size(); i++) {
-			newList.add(lesser.get(i));
-		}
-		// add pivot to newlist
-		newList.add(pivot);
-		// add greaater portion to newlist
-		for (int i = 0; i < greater.size(); i++) {
-			newList.add(greater.get(i));
+			for (int i =0; i< one.size(); i++)
+			{
+				another.set(i,(one.get(i)));
 			}
+			
+		}
+	
+		// Method to build concatenated list: lesser + pivot + greater
+		public  ArrayList<Die> concatenate(ArrayList<Die> lesser,Die pivot,  ArrayList<Die> greater)
+		{
+			// create a newlist to hold the concatenated arraylist
+			ArrayList<Die> newList = new ArrayList<Die>();
+			// add lesser portion to newlist		
+			for (int i = 0; i < lesser.size(); i++) {
+				newList.add(lesser.get(i));
+			}
+			// add pivot to newlist
+			newList.add(pivot);
+			// add greaater portion to newlist
+			for (int i = 0; i < greater.size(); i++) {
+				newList.add(greater.get(i));
+				}
 		
-		return newList;
-	}
-
-
+			return newList;
+		}
 }
