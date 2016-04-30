6f8214ecfae5fa7c615f020ab7ccaad39504ef96
jstange20 <jstange20@gmail.com>
2016-04-29 22:37:02 -0700

    Added files via upload

--- Final-Deliverable/New-UI-Game1-Player/Player.java
+++ Final-Deliverable/New-UI-Game1-Player/Player.java
@@ -1,49 +1,79 @@
-/*
+/**
  * @Author: Jeremy St. Ange, Kartik Gupta, Douglas Porter, Ryan Ditsworth
- * @Version1: 4/15/16
+ * @Version1: April 15, 2016
  * @Version: April 28, 2016
- * Class Description: This class will be the base of player objects. It will be used to store the game stats in 
- * a text file. 
+ * Class Description: This class will be the base of player objects. It will be used to store the players' 
+ *					  statistics in a text file. 
  */
 import java.io.Serializable;
 
 public class Player 
 {
+	// player attributes
 	private int wins = 0;
 	private String playerName = "";
 	
-	// sets the electron number.
-	public void setWins(int incWins){
+	/**
+	 * sets the electron number
+	 * @param incWins
+	 */
+	public void setWins(int incWins)
+	{
 		wins = incWins;
 	}
 	
-	// gets the electron number.
-	public int getWins(){
+	/**
+	 * gets the electron number.
+	 * @return	wins
+	 */
+	public int getWins()
+	{
 		return wins;
 	}
 	
-	// sets the neutron number.
-	public void setPlayerName(String playerName){
+	/**
+	 * sets the neutron number.
+	 * @param playerName
+	 */
+	public void setPlayerName(String playerName)
+	{
 		this.playerName = playerName;
 	}
 	
-	// gets the neutron number.
-	public String getPlayerName(){
+	/**
+	 * gets the neutron number.
+	 * @return	playerName
+	 */
+	public String getPlayerName()
+	{
 		return playerName;
 	}
-	public String toString() {
+	
+	/**
+	 * turns the Player object into a String.
+	 * @return Player
+	 */
+	public String toString() 
+	{
 		return String.format("Player%s, Wins: %d ", playerName, wins);
 	}
 
-	
-	public String serialize() {
+	/**
+	 * serializes the String
+	 */
+	public String serialize() 
+	{
 		return String.format("%s:%d", 
 				 playerName,wins);
 	}
 	
-	
-	// the deserialize is used to read the element information from a file. 
-	public static Player deserialize(String playerData){
+	/**
+	 * reads the element information from the text file.
+	 * @param playerData
+	 * @return newElement
+	 */
+	public static Player deserialize(String playerData)
+	{
 		Player newElement = new Player();
 		
 		String[] data = playerData.split(":");
@@ -52,6 +82,4 @@ public class Player
 		newElement.setWins(Integer.parseInt(data[1]));
 		return newElement;
 	}
-	
-	
 }
